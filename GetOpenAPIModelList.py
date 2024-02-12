5fc6173ed166b178cf688cf984b325885c381283
RevHawk <reghawkins@hotmail.com>
2024-02-12 10:12:18 +0000

    updated

--- GetOpenAPIModelList.py
+++ GetOpenAPIModelList.py
@@ -1,8 +1,9 @@
 from openai import OpenAI
 #import openai
 #client = OpenAI()
-client = OpenAI(api_key="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")
+client = OpenAI(api_key="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")
 #client.api_key = ("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")
 #OpenAI.api_key.encode.__get__ = ("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")
+# Reg - Key2 - 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
 client.models.list()
 print(client.models.list())
